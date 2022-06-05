e4ae1b6be9dfee6bac424bc943d18909d075fd91
obafemi oderanti <thevetdoctor@gmail.com>
2022-06-05 12:21:04 +0100

    implemet admin pages logic implement authentication implement order and dashboard

--- ui/public/admin/js/login.js
+++ ui/public/admin/js/login.js
@@ -1,18 +1,29 @@
+/* eslint-disable no-unused-vars */
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-let emailValue = email['value'];
+let emailValue = email.value;
 let passwordValue = password.value;
 
-function login() {
-    console.log('logging in', emailValue, passwordValue);
-    // console.log('logging in', 'email: ', email.value(), 'password: ', password.value());
+async function login() {
+    const storedEmail = localStorage.getItem('email') || '';
+    const storedPassword = localStorage.getItem('password') || '';
+    const payload = {email: storedEmail, password: storedPassword};
+    console.log('logging in', payload);
+    const res = await fetch('http://localhost:8000/auth/login', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(payload)});
+    const data = await res.json();
+    console.log(data);
+    if(data.status === 200) {
+        // eslint-disable-next-line no-restricted-globals
+        location.replace = 'http://localhost:3000/admin/index.html';
+    }
 }
-login();
+// login();
+console.log(localStorage)
 
-function handleEmail() {
-    console.log(email);
+function handleEmail(emailValue) {
+    localStorage.setItem('email', emailValue);
 }
-function handlePassword() {
-    console.log(password);
+function handlePassword(passwordValue) {
+    localStorage.setItem('password', passwordValue)
 }
